5750464a855a7a3ecac097462be35216d82f826d
MD Nazmur Rashid Rahat <nrzrahat@gmail.com>
2026-08-28 21:52:39 +0600

    cors added into the backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://matrimoy-lk7qn0tav-deep-semantics.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
